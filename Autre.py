4b18ec702ab0b134da3e3d1ab44cc27463331b90
Corentin R <67231057+BenCestMoiQuoi@users.noreply.github.com>
2023-11-10 15:22:10 +0100

    Update Autre.py
    del print

--- Autre.py
+++ Autre.py
@@ -24,14 +24,12 @@ NOM_FILE = f'{path}stock.csv'
 def reorganiser():
     f = open(NOM_FILE, 'r')
     lines = f.readlines()
-    print(lines)
     f.close()
 
     for i in range(len(lines)):
         t1 = lines[i]
         alpha = t1
         alpha_s = t1.split(';')
-        print(i, end='')
         l_para = i
         for j in range(i, len(lines)):
             t2 = lines[j]
@@ -45,11 +43,9 @@ def reorganiser():
             alpha += f'{k}'
             if k != alpha_s[-1]:
                 alpha += ';'
-        print(j, lines[j])
         lines[i], lines[l_para] = alpha, lines[i]
 
     f = open(NOM_FILE, 'w')
     for i in lines:
-        print(i)
         f.writelines(i)
     f.close()
